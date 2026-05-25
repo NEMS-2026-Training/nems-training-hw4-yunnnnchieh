ac2e381d8ff256c86b802d6af3dafd456bd0fc4d
yunnnnchieh <120265058+yunnnnchieh@users.noreply.github.com>
2026-05-25 21:36:36 +0800

    feat: track authentication procedure context

--- detector/context.go
+++ detector/context.go
@@ -9,5 +9,14 @@ var CurrentAuthProcedure AuthProcedureInfo
 // Define every thing you want in this struct,
 // so that you can use them in different message handler
 type AuthProcedureInfo struct {
+	SupiOrSuci         string
+	Supi               string
+	ServingNetworkName string
 	AuthSubsData       models.AuthenticationSubscription
+	Rand               string
+	XresStar           string
+	HxresStar          string
+	Autn               string
+	Kausf              string
+	Kseaf              string
 }
